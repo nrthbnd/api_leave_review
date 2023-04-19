2a5eadbf06fbe1984d013f1ce4c48c188eb517fe
Anastasiia Soloshenkova <asoloshenkova@gmail.com>
2023-04-19 16:17:29 +0300

    Создала файлы для приложения reviews

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reviews.apps.ReviewsConfig',
-    'api.apps.ApiCConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
