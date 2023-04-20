5f05ac024431b58c6973a5feb5d42579d7b6df65
Михаил Приселков <mikhail-upside@yandex.ru>
2023-04-20 19:36:19 +0300

    Написал админку для всех моделей кроме юзер

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from reviews.models import Review, Comment, Title, Genre, Category
 
-# Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug',
+    )
+    list_editable = ('name', 'slug',)
+    empty_value_display = '-пусто-'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug',
+    )
+    list_editable = ('name', 'slug',)
+    empty_value_display = '-пусто-'
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'rating',
+        'description',
+        'category',
+    )
+    list_editable = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'title',
+        'author',
+        'score',
+        'pub_date',
+    )
+    list_editable = ('text', 'score',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'title',
+        'author',
+        'pub_date',
+    )
+    list_editable = ('text',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Category, CategoryAdmin)
