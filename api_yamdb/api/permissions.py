f848de643e26bb6caa7d6211893a9464947318e1
Anastasiia Soloshenkova <asoloshenkova@gmail.com>
2023-04-24 18:29:45 +0300

    Написала пермишены

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,17 +2,20 @@ from rest_framework import permissions
 
 
 class IsAdmin(permissions.BasePermission):
+    """Права доступа администратора"""
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_admin
 
 
 class ReadOnly(permissions.BasePermission):
+    """Права доступа только на чтение"""
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS
 
 
 class IsAuthorOrModeratorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        """Права доступа автора или модератора"""
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.user.is_authenticated:
