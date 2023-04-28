c5b305f654f5db49ec86929052dd3124d66ac3d7
Михаил Приселков <mikhail-upside@yandex.ru>
2023-04-28 18:50:27 +0300

    Fix url review and comment

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -16,10 +16,11 @@ router.register(
     basename='review',
 )
 router.register(
-    r'titles/(?P<titles_id>\d+)/reviews/(?P<review_id>\d+)/comments/',
+    r'titles/(?P<titles_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comment',
 )
+
 
 urlpatterns = [
     path('v1/', include(router.urls)),
