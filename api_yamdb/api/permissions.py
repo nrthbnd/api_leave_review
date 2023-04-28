a95421ccfa5024535022f08063c720bbd4b59ff5
Михаил Приселков <mikhail-upside@yandex.ru>
2023-04-28 18:53:03 +0300

    I don't remember that now

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,14 @@
 from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """Права доступа админу или только на чтение"""
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_staff
 
 
 class IsAdmin(permissions.BasePermission):
