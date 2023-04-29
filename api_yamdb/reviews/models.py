fb3022e076be71c93c137e567242a3fbb2b21024
Anastasiia Soloshenkova <asoloshenkova@gmail.com>
2023-04-29 15:43:36 +0300

    Добавлено поле UniqueConstraint в модель Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -169,6 +169,12 @@ class Review(models.Model):
         ordering = ('-pub_date',)
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_title_author'
+            )
+        ]
 
 
 class Comment(models.Model):
