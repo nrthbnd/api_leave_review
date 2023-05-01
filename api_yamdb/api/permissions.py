07287ccc652c010560d062117470a879b900cb47
Anastasiia Soloshenkova <asoloshenkova@gmail.com>
2023-05-01 16:42:37 +0300

    Ревью 1 корректировка

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -26,6 +26,5 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        else:
-            if request.user.is_authenticated:
-                return request.user.is_admin
+        if request.user.is_authenticated:
+            return request.user.is_admin
