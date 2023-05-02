ecf2a04f2ccbe693f7405d9387ca5de46641028e
Anastasiia Soloshenkova <asoloshenkova@gmail.com>
2023-05-02 10:57:49 +0300

    Ревью 2 корректировка

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,6 +3,7 @@ from django.core.validators import RegexValidator
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
+
 from reviews.models import Category, Comment, Genre, Review, Title, User
 from reviews.validators import validate_username
 
